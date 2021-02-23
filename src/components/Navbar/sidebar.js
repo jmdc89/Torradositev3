f497a9de898b0ef031ebfdd99d1e51a617567066
Jose Manuel <jmdcarq89@gmail.com>
2021-02-23 10:39:31 +0100

    Sidebar css

--- src/components/Navbar/sidebar.js
+++ src/components/Navbar/sidebar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Links from "../../constants/links"
 import { FaTimes } from "react-icons/fa"
+import './sidebar.scss'
 
 const Sidebar = ({isOpen,toggleSidebar}) => {
 
